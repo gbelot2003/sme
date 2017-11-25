b77c628409647d823a1acdb7897f90982a58d058
Gerardo Belot <gbelot2003@hotmail.com>
2017-11-25 10:46:09 -0600

    Agregando seed de users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('users')->delete();
+        
+        \DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Gerardo Belot',
+                'email' => 'gbelot2003@hotmail.com',
+                'password' => '$2y$10$xx7BucB.Qw9VRglppMdFLeJNMFVEy7T0SGuxrCnjM7VYw9eHeTYLK',
+                'remember_token' => NULL,
+                'created_at' => '2017-11-25 16:39:51',
+                'updated_at' => '2017-11-25 16:39:51',
+            ),
+        ));
+        
+        
+    }
+}

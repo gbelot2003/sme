b2b4faaad0c7fc84224c38599d8c403329abd898
Gerardo Belot <gbelot2003@hotmail.com>
2017-11-27 23:47:21 -0600

    removiendo modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,21 +6,4 @@
             <front-component></front-component>
         </div>
     </div>
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="modalTitle"></h4>
-                </div>
-                <div class="modal-body">
-                    <p id="modalMessage"></p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection

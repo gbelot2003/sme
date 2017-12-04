599ccd70224f5fe9fc228a109af0032672283a92
Gerardo Belot <gbelot2003@hotmail.com>
2017-12-04 12:13:51 -0600

    cargando nuevo usuario

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,12 +22,24 @@ class UsersTableSeeder extends Seeder
                 'id' => 1,
                 'name' => 'Gerardo Belot',
                 'email' => 'gbelot2003@hotmail.com',
-                'password' => '$2y$10$xx7BucB.Qw9VRglppMdFLeJNMFVEy7T0SGuxrCnjM7VYw9eHeTYLK',
                 'state_id' => 1,
                 'role' => 'admin',
+                'password' => '$2y$10$xx7BucB.Qw9VRglppMdFLeJNMFVEy7T0SGuxrCnjM7VYw9eHeTYLK',
                 'remember_token' => NULL,
                 'created_at' => '2017-11-25 16:39:51',
                 'updated_at' => '2017-11-25 16:39:51',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Harold Rivera',
+                'email' => 'cconect@hotmail.com',
+                'state_id' => 1,
+                'role' => 'admin',
+                'password' => '$2y$10$OxCGfgZAyNTMC2atFgAivOgo2uWJD9ecqEAn1cUgb9nHGXNqYzHei',
+                'remember_token' => NULL,
+                'created_at' => '2017-12-04 18:12:26',
+                'updated_at' => '2017-12-04 18:12:26',
             ),
         ));
         

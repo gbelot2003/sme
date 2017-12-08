9beb8dd8f53dd8d1173facefcfce5fb39fb0e9ad
Gerardo Belot <gbelot2003@hotmail.com>
2017-12-08 07:41:29 -0600

    removiendo RegisterDataseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(RegistersTableSeeder::class);
+        //$this->call(RegistersTableSeeder::class);
         $this->call(StatesTableSeeder::class);
     }
 }

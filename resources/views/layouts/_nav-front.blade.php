a8571e045fef74367a7bae964c3e49879e0ff89e
Gerardo Belot <gbelot2003@hotmail.com>
2018-02-20 16:57:34 -0600

    agregando links de movimientos

--- resources/views/layouts/_nav-front.blade.php
+++ resources/views/layouts/_nav-front.blade.php
@@ -29,6 +29,9 @@
                 @guest
                 <li><a href="{{ route('login') }}">Login</a></li>
                 @else
+                    <li>
+                        <a href="/home">Formulario</a>
+                    </li>
                     <li><a href="/reportes">Reportes</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -3,7 +3,8 @@
         <div class="navbar-header">
 
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#app-navbar-collapse" aria-expanded="false">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -19,7 +20,7 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                &nbsp;
+
             </ul>
 
             <!-- Right Side Of Navbar -->
@@ -28,9 +29,13 @@
                 @guest
                 <li><a href="{{ route('login') }}">Login</a></li>
                 @else
+                    <li>
+                        <a href="/">Inicio</a>
+                    </li>
                     <li><a href="/reportes">Reportes</a></li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
+                           aria-haspopup="true">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
@@ -45,7 +50,8 @@
                                     Logout
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
